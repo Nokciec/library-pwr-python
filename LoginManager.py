e1f36e514377028574077b03e76ede90403f7b30
nokciec <dominika.poreba@gmail.com>
2018-05-16 17:22:20 +0200

    adawhadbbdbahjbajwbdjawbdadbajdbawhdbajwhdbawbdjwbdawhbdajhbwdhjabwdhjawdbhawbdjaw

--- LoginManager.py
+++ LoginManager.py
@@ -9,13 +9,11 @@ class LoginManager:
 		users_data = DC.get_data(CONSTANTS.USERS_TABLE)
 
 		for user_id in users_data:
-			'''
-			A terrible hack
-			'''
+
 			user = User.empty()
 
 			user.load(CONSTANTS.USERS_TABLE + '/' + user_id)
-			print(user.login, login, user.password, password)
+			
 			if user.login == login:
 				if user.password == password:
 					if user.is_admin:
